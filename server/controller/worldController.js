96066c963bce67744e34b8f5d8136a670b435f92
nineteen17 <102508550+nineteen17@users.noreply.github.com>
2023-03-24 19:28:19 +1300

    Update worldController.js

--- server/controller/worldController.js
+++ server/controller/worldController.js
@@ -34,25 +34,5 @@ module.exports = {
 };
 
 
-//This COde WOnt Work because
 
-// The error message indicates that there is a foreign key constraint on the CountryCode column in the city table. When you try to insert a new city, you also need to provide a valid CountryCode that exists in the country table.
 
-// To fix this issue, you should update your addCity function to include the CountryCode. First, update the addCity function in your backend code:
-
-// const addCity = async (req, res) => {
-//   const q = "INSERT INTO world.city (name) VALUES (?);";
-//   const values = [req.body.name];
-
-//   try {
-//     await pool.query(q, values);
-//     res.status(200).send("Data inserted successfully.");
-//   } catch (error) {
-//     console.error(error);
-//     if (error.code === "ER_PARSE_ERROR") {
-//       res.status(400).send("SQL syntax error.");
-//     } else {
-//       res.status(500).send("Error inserting data into the database.");
-//     }
-//   }
-// };
